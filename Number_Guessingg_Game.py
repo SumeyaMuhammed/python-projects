ebc13eff83b5454ba82ec587ed7a76f407170711
Sumeya Muhammed <sumenaweya@gmail.com>
2024-09-15 00:49:54 -1100

    generate only 1 random num

--- Number_Guessingg_Game.py
+++ Number_Guessingg_Game.py
@@ -1,9 +1,9 @@
 import sys
 import random
 def main():
+    number = random.randint(1, 100)
 
     def guess_num(num):
-        number = random.randint(1, 100)
         if num > number + 15:
             print("your guess is too high.")
         elif num < number - 15:
@@ -16,7 +16,7 @@ def main():
         
     limit = 10
     while limit > 0:
-        user_input = int(input("Guess a number: "))
+        user_input = int(input("Guess the number: "))
         guess_num(user_input)
         limit -= 1
     print("Game Over!")
